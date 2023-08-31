23d5b093b068ae1d340ce59b43653b1860b830a8
Alan Soto <alans@missionready.com>
2023-08-31 16:53:00 +1200

    Commit N3

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,16 @@
-const express = require("express");
-const cors = require("cors");
-const apiRoutes = require("./routes/api");
-const multer = require("multer");
+const express = require('express')
+const cors = require('cors')
+const apiRoutes = require('./routes/api')
+const multer = require('multer')
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-// Set up multer for handling file uploads
-const storage = multer.memoryStorage(); // Store files in memory as buffers
-const upload = multer({ storage: storage }); // Only accept file uploads with the image name.
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
-app.use(upload.single("image")); //
+app.use(upload.single('image'))
+app.use('/api', apiRoutes)
 
-app.use("/api", apiRoutes);
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-module.exports = app;
+module.exports = app
